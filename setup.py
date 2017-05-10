8c63845f8ca9a085fe5c0ebe46f1f90ec31b65ed
Tal Wrii <git@tatw.name>
2017-05-10 17:09:36 +0100

    Include setup.py
    This includes magic to generate antlr files when installed.
    
    This works with `setup.py develop`, `setup.py install` and `pip`.
    It does not, however, work with tox. (http://stackoverflow.com/questions/43897581/python-packaging-generate-a-python-file-at-installation-time-have-this-work-wi)

---
+++ setup.py
@@ -0,0 +1,49 @@
+# make code as python 3 compatible as possible
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+import subprocess
+import setuptools
+import os.path
+import distutils.core
+
+from setuptools.command.develop import develop
+from setuptools.command.install import install
+
+
+# Build anltr files on installation
+#   this is such a mess... it looks like there are
+#   no common steps to develop and install
+
+class AntlrDevelopCommand(develop):
+    def run(self):
+        compile_grammar()
+        develop.run(self)
+
+class AntlrInstallCommand(install):
+    def run(self):
+        compile_grammar()
+        install.run(self)
+
+def compile_grammar():
+    here = os.path.dirname(__file__) or '.'
+    package_dir = os.path.join(here, 'latex2sympy')
+    subprocess.check_output(['antlr4',  'PS.g4', '-o', 'gen'], cwd=package_dir)
+
+setuptools.setup(
+    name='latex2sympy',
+    version=0.1,
+    author='august.codes',
+    author_email='augustt198@gmail.com',
+    description='Parse latex markup into sympy: suitable for programmatic modifcation',
+    license='GPLv3',
+    keywords='MIT',
+    url='',
+    packages=['latex2sympy'],
+    classifiers=[
+    ],
+    install_requires=['antlr-ast',  'sympy'],
+    cmdclass=dict(
+        install=AntlrInstallCommand,
+        develop=AntlrDevelopCommand),
+    test_suite='nose.collector'
+)
